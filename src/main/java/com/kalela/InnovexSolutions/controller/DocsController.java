0b34d77c63ed0670f59f7fd7f52d6d262f03f5f4
Kalela <philipkalela@gmail.com>
2020-10-09 12:42:40 +0300

    mend

--- src/main/java/com/kalela/InnovexSolutions/controller/DocsController.java
+++ src/main/java/com/kalela/InnovexSolutions/controller/DocsController.java
@@ -1,5 +1,6 @@
 package com.kalela.InnovexSolutions.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class DocsController {
 
+    @GetMapping
     public String homePage() {
         return "Hello there";
     }
